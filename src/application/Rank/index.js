202448bbbfe6b1b7e344169100ce85a79f94484a
hexiaolong16735 <hexiaolong16735@ipalfish.com>
2022-05-27 13:23:49 +0800

    feat: update

--- src/application/Rank/index.js
+++ src/application/Rank/index.js
@@ -1,7 +1,20 @@
-import React from "react";
+// import React from "react";
 
-function Rank(props) {
-  return <div>Rank</div>;
+// function Rank(props) {
+//   return <div>Rank</div>;
+// }
+
+// export default React.memo(Rank);
+import AddTask from "./components/AddTask.js";
+import TaskList from "./components/TaskList.js";
+import { TasksProvider } from "./components/TasksContext.js";
+
+export default function TaskApp() {
+  return (
+    <TasksProvider>
+      <h1>Day off in Kyoto</h1>
+      <AddTask />
+      <TaskList />
+    </TasksProvider>
+  );
 }
-
-export default React.memo(Rank);
